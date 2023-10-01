0363f77e00e5623ccc1e530fb88f51b8646451f6
joel <joel-zavala17@hotmail.com>
2023-10-01 11:08:55 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+
+
+
+
+writeCards(['Guadalupe', 'Ollie', 'Aki'], "birthday");
+
+  function writeCards(names, event) {
+    let message=[]
+    for (let i = 0; i < names.length; i++) {
+      message.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+      
+    } return message ;
+  }
+
+
+  
+  function countDown(integer) {
+    while (integer >= 0) {
+      console.log(integer);
+      integer--;
+    }
+  }
+  
+  countDown(10);
